b4054db20319e8bd66ab8e230457c1676ad01ea7
Facu Olivieri <facu.olivieri7@gmail.com>
2026-07-30 20:32:30 -0300

    Client Controller completo para CRUD

--- src/main/java/com/pasteleriaPri/Pasteleria/controller/ClientController.java
+++ src/main/java/com/pasteleriaPri/Pasteleria/controller/ClientController.java
@@ -1,8 +1,6 @@
 package com.pasteleriaPri.Pasteleria.controller;
 
 import com.pasteleriaPri.Pasteleria.dto.ClientDTO;
-import com.pasteleriaPri.Pasteleria.entity.Client;
-import com.pasteleriaPri.Pasteleria.helpers.Mapper;
 import com.pasteleriaPri.Pasteleria.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +23,25 @@ public class ClientController {
         return ResponseEntity.created(URI.create("/api/pasteleria/client")).body(newClientDTO);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.findById(id).get());
+    }
+
     @GetMapping
     public ResponseEntity<List<ClientDTO>> getAllClients() {
         return ResponseEntity.ok(clientService.findAll());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        clientService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientDTO> updateClient(@PathVariable Long id, @RequestBody ClientDTO clientDTO) {
+        return ResponseEntity.ok(clientService.update(id, clientDTO));
+    }
 
 }
